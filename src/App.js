1f54b46f8b9afda1aa99d5605b68c5500d890b0d
Vimal Vinayraj <vimalvinayraj@gmail.com>
2024-07-14 13:10:21 +0400

    understanding useRef

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
-
-import React, { useReducer} from "react";
-
-const initialState = {num: 0, dhanu: true}
-
-const reducer = (state, action) => {
-  switch(action.type) {
-    case "INCREMENT":
-      return {...state, num: state.num + 1 }
-    case "TOGGLE":
-      return {...state, dhanu: !state.dhanu}
-      default:
-        return state;
-  }
-  
-}
+import React, {useRef} from "react";
 
 const App = () => {
-  const[state, dispatch] = useReducer(reducer, initialState)
 
-  const increment = () => {
-   dispatch({type: "INCREMENT"})
-   dispatch({type: "TOGGLE"})
+  const inputRef = useRef(null)
+
+  const handkeChange = () => {
+    inputRef.current.focus();
   }
 
-  return(
-    <> 
-    <h1>React Hooks</h1>
+  return (
+    <>
+    <h1> Vimal </h1>
+    <label> UserName</label>
+    <input 
+    type="name"
+    placeholder="Enter the UserName"
+    ref={inputRef}
+    />
 
-    <h2>{state.num}</h2>
-  
-    <button onClick={increment}> Increment </button>
-    {state.dhanu ? <h1> Hai Vidyu </h1> : ''}
+    <button onClick={handkeChange}> Submit </button>
     </>
-    
   )
 }
 
